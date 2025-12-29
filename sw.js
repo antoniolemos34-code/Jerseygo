65f209f66bfa599b07422e5234c126cea52a1fdf
antoniolemos34-code <antoniolemos34@gmail.com>
2025-12-29 05:12:02 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+const CACHE = "jerseygo-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
